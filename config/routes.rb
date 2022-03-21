8217b8053dca03d1b03f16057d35ab67e9943ffa
Tim Jedrek <tim@timjedrek.com>
2022-03-20 22:51:33 -0500

    put complaints index as the root (for now...)

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
-  root 
+  root "complaints#index"
 
   get "/complaints", to: "complaints#index"
   get "/users", to: "users#index"
